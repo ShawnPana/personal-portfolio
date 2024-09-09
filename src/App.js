c6a55eb7fb19cc902f830b3d624818e4e4f1cb95
ShawnPana <spana@ucsd.edu>
2024-09-09 12:06:20 -0700

    moved flask changes to flask branch

--- src/App.js
+++ src/App.js
@@ -3,59 +3,11 @@ import { useEffect } from 'react'
 import Home from './components/Home'
 import Lanyard from './components/Mobile'
 import SentimentAnalysis from './components/Sentiment';
-import Test from './components/Test';
 
 function App() {
   return (
-    // <Test />
     <Home />
   );
 }
 
 export default App;
-
-// import React, { useState, useEffect } from 'react';
-
-// function App() {
-//   const [data, setData] = useState({});
-//   const [isLoaded, setIsLoaded] = useState(false);
-
-//   useEffect(() => {
-//     fetch("http://127.0.0.1:5000/dick", {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json',
-//       },
-//       mode: 'cors',
-//     })
-//       .then(res => res.json())
-//       .then(data => {
-//         setData(data);
-//         setIsLoaded(true);
-//         console.log(data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching data:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       {!isLoaded ? (
-//         <div>Loading...</div>
-//       ) : (
-//         <div>
-//           <h1>Members:</h1>
-//           <ul>
-//             {data.man.map((member, index) => (
-//               <li key={index}>{member}</li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
