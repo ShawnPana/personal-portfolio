241f6ee3a598b8bdd4919be2699952394372dbba
ShawnPana <spana@ucsd.edu>
2024-09-09 13:59:23 -0700

    changed logos, working on room and detailing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import { useEffect } from 'react'
 import Home from './components/Home'
-import Lanyard from './components/Mobile'
-import SentimentAnalysis from './components/Sentiment';
 
 function App() {
   return (
