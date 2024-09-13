2f72506eda70851684ca63331b2ab7411685517e
ShawnPana <spana@ucsd.edu>
2024-09-13 13:00:08 -0700

    added loading screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './components/Home'
+// import Tree from './components/Tree'
 
 function App() {
   return (
     <Home />
+    // <Tree />
   );
 }
 
--- src/components/Tree.js
+++ src/components/Tree.js
@@ -9,7 +9,6 @@ export default function Tree() {
     const refContainer = useRef(null);
     
     useEffect(() => {
-        // scene setup
         document.body.style.overflow = 'hidden';
         var scene = new THREE.Scene();
         var renderer = new THREE.WebGLRenderer();
@@ -19,23 +18,18 @@ export default function Tree() {
         const ambientLight = new THREE.AmbientLight(0xffffff, 2);
         scene.add(ambientLight);
         var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 35;
-        var gltfLoader = new GLTFLoader();
-        const fontLoader = new FontLoader();
-
-        // PointerLockControls setup
+        camera.position.z = 50;
+        const manager = new THREE.LoadingManager();
+        const gltfLoader = new GLTFLoader(manager);
+        const fontLoader = new FontLoader(manager);
         const controls = new PointerLockControls(camera, document.body);
         scene.add(controls.getObject());
-
-        // Movement setup
         let moveForward = false;
         let moveBackward = false;
         let moveLeft = false;
         let moveRight = false;
-        
         const velocity = new THREE.Vector3();
         const direction = new THREE.Vector3();
-
         const onKeyDown = function ( event ) {
             switch ( event.code ) {
                 case 'KeyW':
@@ -54,7 +48,6 @@ export default function Tree() {
                     break;
             }
         };
-
         const onKeyUp = function ( event ) {
             switch ( event.code ) {
                 case 'KeyW':
@@ -73,49 +66,14 @@ export default function Tree() {
                     break;
             }
         };
-
         document.addEventListener( 'keydown', onKeyDown );
         document.addEventListener( 'keyup', onKeyUp );
-
-        // Lock and unlock controls
         const lockControls = () => {
             controls.lock();
         };
         document.addEventListener('click', lockControls);
-
-        let model;
-        gltfLoader.load('/models/ps1testtt5.glb', (gltf) => {
-            model = gltf.scene;
-            scene.add(model);
-            model.position.set(-17.6206, -11.1618, -7.03967);
-        });
-
-        fontLoader.load( "/fonts/helvetiker_regular.typeface.json", function (font) {
-            const textGeo = new TextGeometry( "organregistry.org", {
-                font: font,
-                size: 1,
-                depth: 0.01,
-            } );  
-            const textMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-            const mesh = new THREE.Mesh( textGeo, textMaterial );
-            scene.add( mesh );
-        });
-
-        let mixer;
-        let hands;
-        gltfLoader.load('/models/hands.glb', (gltf) => {
-            hands = gltf.scene;
-            scene.add(hands);
-            hands.scale.set(6, 6, 6);
-
-            mixer = new THREE.AnimationMixer(hands);
-            const action = mixer.clipAction(gltf.animations[0]);
-            action.play();
-        });
-
         const keys = { w: false, a: false, s: false, d: false };
         const moveSpeed = 0.1;
-
         window.addEventListener('keydown', (e) => {
             if (e.key.toLowerCase() in keys) {
                 keys[e.key.toLowerCase()] = true;
@@ -126,6 +84,77 @@ export default function Tree() {
                 keys[e.key.toLowerCase()] = false;
             }
         });
+
+
+        let model;
+        let treeMixer;
+        gltfLoader.load('/models/ps1testtt5.glb', (gltf) => {
+            model = gltf.scene;
+
+            // Compute the bounding box of the model
+            const box = new THREE.Box3().setFromObject(model);
+
+            // Calculate the center of the bounding box
+            const center = new THREE.Vector3();
+            box.getCenter(center);
+
+            // Translate the model so its origin is at (0, 0, 0)
+            model.position.sub(center);
+
+            scene.add(model);
+
+            treeMixer = new THREE.AnimationMixer(model);
+            for (let i = 0; i < gltf.animations.length; i++) {
+                const action = treeMixer.clipAction(gltf.animations[i]);
+                action.play();
+            }
+        });
+
+        let text;
+        fontLoader.load("/fonts/helvetiker_regular.typeface.json", function (font) {
+            const textGeo = new TextGeometry("organregistry.org", {
+                font: font,
+                size: 1,
+                depth: 0.01,
+            });
+        
+            // Compute the bounding box of the text geometry
+            textGeo.computeBoundingBox();
+        
+            // Get the center of the bounding box
+            const boundingBox = textGeo.boundingBox;
+            const centerX = (boundingBox.max.x - boundingBox.min.x) / 2;
+            const centerY = (boundingBox.max.y - boundingBox.min.y) / 2;
+            const centerZ = (boundingBox.max.z - boundingBox.min.z) / 2;
+        
+            // Translate the geometry to center it
+            textGeo.translate(-centerX, -centerY, -centerZ);
+        
+            const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+            const mesh = new THREE.Mesh(textGeo, textMaterial);
+        
+            // Set position of the mesh in the scene
+            
+            mesh.position.set(8, -0.25, 10);
+            scene.add(mesh);
+        
+            text = mesh;
+        });
+        
+        let mixer;
+        let hands;
+        gltfLoader.load('/models/hands.glb', (gltf) => {
+            hands = gltf.scene;
+            hands.rotation.y = Math.PI;
+            // camera.add(hands);
+            // scene.add(camera);
+            hands.scale.set(6, 6, 6);
+
+            mixer = new THREE.AnimationMixer(hands);
+            const action = mixer.clipAction(gltf.animations[0]);
+            action.play();
+        });
+
 
         var animate = function () {
             requestAnimationFrame(animate);
@@ -155,16 +184,41 @@ export default function Tree() {
             if (mixer) {
                 mixer.update(0);
                 mixer.setTime(5);
+
+                // mixer.update(delta);
             }
-            if (hands){
-                hands.position.copy(controls.getObject().position);
-                hands.rotation.y = controls.getObject().rotation.y + Math.PI;
-                hands.rotation.x = controls.getObject().rotation.x;
+            if (treeMixer) {
+                // treeMixer.update(0);
+                // treeMixer.setTime(0);
+                
+
+                treeMixer.update(delta);
+            }
+            if (text){
+                text.lookAt(camera.position);
             }
 
             renderer.render(scene, camera);
         };
-        animate();
+
+        manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+            console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+            document.getElementById('loadingOverlay').style.display = 'block';
+        };
+
+        manager.onLoad = function ( ) {
+            document.getElementById('loadingOverlay').style.display = 'none';
+            animate();
+            scene.visible = true;
+        };
+
+        manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+            console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+        };
+
+        manager.onError = function ( url ) {
+            console.log( 'There was an error loading ' + url );
+        };
 
         return () => {
             window.removeEventListener('keydown', (e) => {
@@ -189,6 +243,28 @@ export default function Tree() {
     }, []);
 
     return (
-        <div ref={refContainer}></div>
+        <div ref={refContainer}>
+            {
+                <div
+                    id="loadingOverlay"
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100vw',
+                        height: '100vh',
+                        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                        color: '#fff',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        fontSize: '2rem',
+                        zIndex: 10,
+                    }}
+                >
+                    loading...  
+                </div>
+            }
+          </div>
     )
 }
